807b1bab6af5b1da6091395bd084a6748c98cb6b
NicolasMairone <mairone.nicolas@gmail.com>
2023-10-04 00:03:16 -0300

    Actualizacion del formulario marca
    Actualizamos estilos y agregamos alerts

--- taller/src/components/Forms/formMarca.js
+++ taller/src/components/Forms/formMarca.js
@@ -1,25 +1,40 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import Alert from 'react-bootstrap/Alert';
 
 export function FormMarca() {
-  const [validated, setValidated] = useState(false);
+  const [showInsertAlert, setShowInsertAlert] = useState(false);
+  const [showSaveAlert, setShowSaveAlert] = useState(false);
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  const handleInsertClick = () => {
+    setShowInsertAlert(true);
+    setTimeout(() => {
+      setShowInsertAlert(false);
+    }, 1000);
+  };
 
-    setValidated(true);
+  const handleSaveClick = () => {
+    setShowSaveAlert(true);
+    setTimeout(() => {
+      setShowSaveAlert(false);
+    }, 1000);
+  };
+
+  const handleDeleteClick = () => {
+    setShowDeleteAlert(true);
+    setTimeout(() => {
+      setShowDeleteAlert(false);
+    }, 1000);
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit} className="Forms"> 
+    <Form className="Forms">
+
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label className="custom-label">Nombre</Form.Label>
@@ -41,77 +56,55 @@ export function FormMarca() {
           />
           <Form.Control.Feedback></Form.Control.Feedback>
         </Form.Group>
-        {/* 
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-        */}
       </Row>
-      {/* 
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
-        
-      </Row>
-      */}
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label={<span style={{ color: 'black' }}>Aceptar terminos y condiciones</span>}
-          feedback="Debes estar de acuerdo antes de registrar una marca"
-          feedbackType="invalid"
-        />
-      </Form.Group>
       <Row className="div-buttons">
         <Col className="custom-col">
-          <Button type="submit">Insertar Marca</Button>
+          <Button variant="primary" onClick={handleInsertClick}>
+            Ingresar Marca
+          </Button>
+          <br></br>
+          <br></br>
+          {/* Renderiza la alerta solo si showInsertAlert es true */}
+          {showInsertAlert && (
+            <Alert variant="success" className="custom-alert">
+              Marca ingresada exitosamente
+            </Alert>
+          )}
         </Col>
-
         <Col className="custom-col">
-          <Button type="submit">Guardar Cambios</Button>
+          <Button variant="primary" onClick={handleSaveClick}>
+            Guardar Cambios
+          </Button>
+          <br></br>
+          <br></br>
+          {/* Renderiza la alerta solo si showSaveAlert es true */}
+          {showSaveAlert && (
+            <Alert variant="success" className="custom-alert">
+              Marca actualizada exitosamente
+            </Alert>
+          )}
         </Col>
-        
         <Col className="custom-col">
-          <Button type="submit">Eliminar Marca</Button>
-        </Col>  
-
+          <Button variant="primary" onClick={handleDeleteClick}>
+            Eliminar Marca
+          </Button>
+          <br></br>
+          <br></br>
+          {/* Renderiza la alerta solo si showDeleteAlert es true */}
+          {showDeleteAlert && (
+            <Alert variant="success" className="custom-alert">
+              Marca eliminada exitosamente
+            </Alert>
+          )}
+        </Col>
         <Col className="custom-col">
-          <Button type="submit">Listar Marcas</Button>
+          <Button variant="primary">
+            Listar Marcas
+          </Button>
         </Col>
       </Row>
     </Form>
   );
 }
 
-export default FormMarca;
+export default FormMarca;
