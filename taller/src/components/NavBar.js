06ba5b011efb2f1d65447d184d621478bb6f7d12
NicolasMairone <mairone.nicolas@gmail.com>
2023-10-04 00:00:30 -0300

    Modificacion del NavBar
    Agregamos una imagen del logo del taller y eliminamos la seccion "Home"

--- taller/src/components/NavBar.js
+++ taller/src/components/NavBar.js
@@ -4,16 +4,25 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
+import { Image } from "react-bootstrap";
+import logoImage from "../../src/imagen/logo.png";
+
 
 export function NavBar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">LOS SANTOS CUSTOM</Navbar.Brand>
+        <Navbar.Brand href="/home">
+          <img
+            src={logoImage} 
+            alt="Logo"
+            width="100"
+            height="40"
+          /> 
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+          <Nav className="me-auto">           
             <NavDropdown title="Opciones" id="basic-nav-dropdown">
               <NavDropdown.Item as={Link} to="/marca">Marca</NavDropdown.Item>
               <NavDropdown.Item as={Link} to="/modelo">Modelo</NavDropdown.Item>
