825b584357e494b0bf9a9559c2edffe142cc2312
Lucas <mazalucas@hotmail.com>
2023-10-20 18:16:30 -0300

    fix(api): fixeo de data return de axios

--- taller/src/api/Marca.Controller.js
+++ taller/src/api/Marca.Controller.js
@@ -14,21 +14,21 @@ import { SpringBoot_Api } from "../app.config";
  * @returns {Promise<Array<any>>} El listado de clientes.
  */
 
-export async function buscarMarca() {
-    try {
-      const { dataM } = await axios({
-        method: "GET",
-        url: `${SpringBoot_Api}/marcas`,
-      });
-  
-      return dataM;
-    } catch (e) {
-      console.error(e);
-      return [];
-    }
-  }
+export async function buscarMarcas() {
+  try {
+    const { data: dataM } = await axios({
+      method: "GET",
+      url: `${SpringBoot_Api}/marcas`,
+    });
 
-  /**
+    return dataM;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+
+/**
  * Inserta una nueva marca en el sistema.
  *
  * @param {Marca} marca La marca a insertar.
@@ -68,20 +68,20 @@ export async function crearMarca(marca) {
  * @returns {Promise<boolean>} Si se eliminó o no.
  */
 export async function eliminarMarca(id) {
-    try {
-      await axios({
-        method: "DELETE",
-        url: `${SpringBoot_Api}/marcas/${id}`,
-      });
-  
-      return true;
-    } catch (e) {
-      console.error(e);
-      return false;
-    }
-  }
+  try {
+    await axios({
+      method: "DELETE",
+      url: `${SpringBoot_Api}/marcas/${id}`,
+    });
 
-  /**
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+/**
  * Edita a una marca del dominio.
  *
  * @param {Marca} marca La marca a editar.
@@ -89,16 +89,16 @@ export async function eliminarMarca(id) {
  * @returns {Promise<any>} La marca editada.
  */
 export async function editarMarca(marca) {
-    try {
-      const { data: marc } = await axios({
-        method: "PATCH",
-        url: `${SpringBoot_Api}/marcas/${marca.id}`,
-        data: marca,
-      });
-  
-      return marc;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
+  try {
+    const { data: marc } = await axios({
+      method: "PATCH",
+      url: `${SpringBoot_Api}/marcas/${marca.id}`,
+      data: marca,
+    });
+
+    return marc;
+  } catch (e) {
+    console.error(e);
+    return null;
   }
+}
