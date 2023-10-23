9575cf6313a9b0ad402154a2f42f75fc1ffdadaa
NicolasMairone <mairone.nicolas@gmail.com>
2023-10-23 14:50:05 -0300

    creacion de formulario de servicios

--- taller/src/components/Forms/formServicio.js
+++ taller/src/components/Forms/formServicio.js
@@ -1,110 +1,148 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Alert from 'react-bootstrap/Alert';
+import { useServicioContext } from '../../context/ServicioContextProvider';
+import ServicioTable from "../Tables/ServicioTable"
+import {crearServicio} from "../../api/Servicio.Controller"
 
-export function FormServicio() {
-  const [showInsertAlert, setShowInsertAlert] = useState(false);
-  const [showSaveAlert, setShowSaveAlert] = useState(false);
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+const formatOptionWithDescription = ({ value, label, description }) => (
+  <div className="flex flex-col">
+    <div>{label}</div>
+    <div className="text-sm text-slate-500">{description}</div>
+  </div>
+);
 
-  const handleInsertClick = () => {
-    setShowInsertAlert(true);
-    setTimeout(() => {
-      setShowInsertAlert(false);
-    }, 1000);
+export default function Servicio({something}) {
+  const {servicios, setServicios, removerServicios, agregarServicio} = useServicioContext();
+
+  const [name, setName] = useState("");
+  const nameRef = useRef(name);
+  const [descripciones, setDescripciones] = useState("");
+  const descripcionesRef = useRef(descripciones);
+
+  const [isLoading, setIsLoading] = useState("");
+
+  const [canCreate, setCanCreate] = useState(false);
+
+  const validateInputFields = (name) => {
+    if (!name?.length) return false;
+
+    return true;
   };
 
-  const handleSaveClick = () => {
-    setShowSaveAlert(true);
-    setTimeout(() => {
-      setShowSaveAlert(false);
-    }, 1000);
+  const handleServiceCreation = async () => {
+    // Armar cuerpo requerido para creacion.
+    const body = {
+      name: name ?? null,
+      descripciones: descripciones ?? null,
+    };
+
+    setIsLoading(true);
+    setCanCreate(false);
+
+    try {
+      // Crear la nueva orden.
+      const servicio = await crearServicio(body);
+
+      // Agregar la nueva orden a la lista de ordenes.
+      agregarServicio(servicio);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // Resettear estados de control de flujo.
+    setIsLoading(false);
+    setCanCreate(true);
+    clearFormFields();
   };
 
-  const handleDeleteClick = () => {
-    setShowDeleteAlert(true);
-    setTimeout(() => {
-      setShowDeleteAlert(false);
-    }, 1000);
+  /**
+   * Limpia los campos del formulario.
+   */
+  const clearFormFields = () => {
+    // Limpiar campos usando las referencias al DOM.
+    nameRef.current.value = "";
+    descripcionesRef.current.value = "";
+
+    // Limpiar estados.
+    setDescripciones("");
+    setName("");
+    setCanCreate(false);
   };
 
   return (
-    <Form className="Forms">
+    <div className="grid grid-cols-2 gap-4 w-full mx-4">
+      <div className="w-full p-4 bg-slate-200 rounded-xl flex flex-col gap-y-2">
+        <h1 className="text-xl">Servicios</h1>
+        <div className="w-full grid grid-cols-2 gap-x-2">
+          <div className="w-full py-1">
+            <span className="text-sm text-slate-600 p-0">
+              Nombre <span className="text-red-400">*</span>
+            </span>
+            <input
+              isClearable
+              isSearchable
+              placeholder="Introduzca un nombre"
+              ref={nameRef}
+              onChange={(e) => {
+                const value = e.target?.value;
 
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label className="custom-label">Name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Introduzca nombre del servicio"
-            defaultValue=""
-          />
-          <Form.Control.Feedback></Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label className="custom-label">Descripcion</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Introduzca una descripcion del servicio"
-            defaultValue=""
-          />
-          <Form.Control.Feedback></Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row className="div-buttons">
-        <Col className="custom-col">
-          <Button variant="primary" onClick={handleInsertClick}>
-            Ingresar Marca
-          </Button>
-          <br></br>
-          <br></br>
-          {/* Renderiza la alerta solo si showInsertAlert es true */}
-          {showInsertAlert && (
-            <Alert variant="success" className="custom-alert">
-              Marca ingresada exitosamente
-            </Alert>
-          )}
-        </Col>
-        <Col className="custom-col">
-          <Button variant="primary" onClick={handleSaveClick}>
-            Guardar Cambios
-          </Button>
-          <br></br>
-          <br></br>
-          {/* Renderiza la alerta solo si showSaveAlert es true */}
-          {showSaveAlert && (
-            <Alert variant="success" className="custom-alert">
-              servicio actualizado exitosamente
-            </Alert>
-          )}
-        </Col>
-        <Col className="custom-col">
-          <Button variant="primary" onClick={handleDeleteClick}>
-            Eliminar servicio
-          </Button>
-          <br></br>
-          <br></br>
-          {/* Renderiza la alerta solo si showDeleteAlert es true */}
-          {showDeleteAlert && (
-            <Alert variant="success" className="custom-alert">
-              servicio eliminada exitosamente
-            </Alert>
-          )}
-        </Col>
-        <Col className="custom-col">
-          <Button variant="primary">
-            Listar servicio
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+                setName(value);
+
+                // Usar directamente 'value' para la validación.
+                setCanCreate(validateInputFields(value));
+              }}
+            ></input>
+
+          </div>
+          <div className="w-full col-span-2">
+            <span className="text-sm text-slate-600 p-0">Descripcion</span>
+            <textarea
+              rows={5}
+              className="w-full rounded-md p-2"
+              placeholder="Opcionalmente, provea una descripcion adicional..."
+              style={{
+                resize: "none",
+              }}
+              ref={descripcionesRef}
+              onChange={(e) => {
+                const value = e.target?.value;
+
+                setDescripciones(value?.length ? value : null);
+
+              }}
+            ></textarea>
+          </div>
+        </div>
+        <div className="w-full grid grid-cols-3 gap-x-2">
+          <button
+            className={`w-full p-2 ${
+              canCreate ? "bg-blue-500" : "bg-blue-300"
+            } rounded-xl text-white font-semibold col-span-2`}
+            disabled={!canCreate}
+            onClick={handleServiceCreation}
+          >
+            Crear Nuevo
+          </button>
+          <button
+            className="w-full p-2 bg-orange-500 rounded-xl text-white font-semibold"
+            onClick={clearFormFields}
+          >
+            Limpiar Todo
+          </button>
+        </div>
+      </div>
+      <div className="w-[95%]">
+        <ServicioTable
+          servicio={servicios}
+          removerServicios={removerServicios}
+          setServicios={setServicios}
+        />
+      </div>
+    </div>
   );
 }
-
-export default FormServicio;
