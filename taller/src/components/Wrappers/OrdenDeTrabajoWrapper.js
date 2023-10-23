4b14e5ac26e2532db10de3c2efe15f031223c094
Lucas <mazalucas@hotmail.com>
2023-10-23 01:29:15 -0300

    feat(orden): implementada interfaz orden v2
    Creacion de una interfaz beta de Ordenes de Trabajo con validaciones correspondientes a las historias de usuario detalladas en la entrega.

--- taller/src/components/Wrappers/OrdenDeTrabajoWrapper.js
+++ taller/src/components/Wrappers/OrdenDeTrabajoWrapper.js
@@ -1,17 +1,17 @@
-import FormOrdenDeTrabajo from "../Forms/formOrdenDeTrabajo"
+//import FormOrdenDeTrabajo from "../Forms/formOrdenDeTrabajo";
 import { AutomovilContextProvider } from "../../context/AutomovilContextProvider";
 import { ServicioContextProvider } from "../../context/ServicioContextProvider";
-import { OrdenContextProvider } from "../../context/OrdenContextProvider"; 
+import { OrdenContextProvider } from "../../context/OrdenContextProvider";
+import OrdenDeTrabajo from "../Forms/OrdenDeTrabajo";
 
 export default function OrdenDeTrabajoWrapper() {
   return (
     <ServicioContextProvider>
-        <AutomovilContextProvider>
-          <OrdenContextProvider>
-            <FormOrdenDeTrabajo />
-          </OrdenContextProvider>
-        </AutomovilContextProvider>
+      <AutomovilContextProvider>
+        <OrdenContextProvider>
+          <OrdenDeTrabajo />
+        </OrdenContextProvider>
+      </AutomovilContextProvider>
     </ServicioContextProvider>
-
   );
 }
