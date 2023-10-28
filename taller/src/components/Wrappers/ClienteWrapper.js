081a18b6a406a2a7dbe874c4ceb7dc4fca41d3e9
Lucas <mazalucas@hotmail.com>
2023-10-28 15:49:50 -0300

    feat(cliente): nueva tabla test cliente

--- taller/src/components/Wrappers/ClienteWrapper.js
+++ taller/src/components/Wrappers/ClienteWrapper.js
@@ -1,10 +1,12 @@
 import FormCliente from "../Forms/formCliente";
 import { ClienteContextProvider } from "../../context/ClienteContextProvider";
+import Cliente from "../Forms/Cliente";
 
 export default function ClienteWrapper() {
   return (
     <ClienteContextProvider>
       <FormCliente />
+      {/* <Cliente /> */}
     </ClienteContextProvider>
   );
 }
