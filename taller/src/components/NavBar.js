afef5951674e0bf7bd956a536936848f696c26ac
Lucas <mazalucas@hotmail.com>
2023-10-28 15:50:04 -0300

    refactor(*): apunte a nuevos componentes, renombrados

--- taller/src/components/NavBar.js
+++ taller/src/components/NavBar.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom"; // Importa Link desde react-router-dom
 import { Image } from "react-bootstrap";
 import logoImage from "../../src/imagen/logo.png";
 
-
 export function NavBar() {
   return (
-    <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+    <Navbar
+      bg="dark"
+      data-bs-theme="dark"
+      expand="lg"
+      className="bg-body-tertiary"
+    >
       <Container>
         {/*Imagen en el NavBar*/}
-        <Navbar.Brand href="/home" style={{ marginLeft: '-100px' }}>
-          <img
-            src={logoImage} 
-            alt="Logo"
-            width="120"
-            height="50"
-          /> 
+        <Navbar.Brand href="/home" style={{ marginLeft: "-100px" }}>
+          <img src={logoImage} alt="Logo" width="120" height="50" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">           
-            <NavDropdown title="Opciones" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/marca">CRUD Marca</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/modelo">CRUD Modelo</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/automovil">CRUD Automovil</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/cliente">CRUD Cliente</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/ordenTrabajo">CRUD OrdenTrabajo</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/servicio">CRUD Servicio</NavDropdown.Item>
-
+          <Nav className="me-auto">
+            <NavDropdown title="Administrar" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/marca">
+                Marcas de Automovil
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/modelo">
+                Modelos de Automovil
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/automovil">
+                Automoviles
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/cliente">
+                Clientes
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/ordenTrabajo">
+                Ordenes de Trabajo
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/servicio">
+                Servicios
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
